06499f5425bab48375b2c7ec7b8c0e91e4f64af9
Joseph Moniz <joseph.moniz@gmail.com>
2014-11-29 10:43:39 -0800

    version bump

--- project/Build.scala
+++ project/Build.scala
@@ -17,11 +17,11 @@ object Build extends Build {
     .settings(
       name                  := "riak-kv",
       organization          := "com.plasmaconduit",
-      version               := "0.3.0",
+      version               := "0.4.0",
       scalaVersion          := "2.11.2",
       licenses              += ("MIT", url("http://opensource.org/licenses/MIT")),
       resolvers           += "Plasma Conduit Repository" at "http://dl.bintray.com/plasmaconduit/releases",
-      libraryDependencies += "com.plasmaconduit" %% "rx-netty-scala" % "0.16.0",
+      libraryDependencies += "com.plasmaconduit" %% "rx-netty-scala" % "0.17.0",
       libraryDependencies += "com.plasmaconduit" %% "json" % "0.7.0",
       libraryDependencies += "com.plasmaconduit" %% "url" % "0.4.0"
     )
